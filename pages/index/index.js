6aabe691a3f3443fe94b01eeec55d3e4e189dbc5
Your Name <you@example.com>
2017-08-23 23:58:24 +0800

    使用 chooseLocation

--- pages/index/index.js
+++ pages/index/index.js
@@ -32,17 +32,22 @@ Page({
     })
   },
 
+  chooseLocation() {
+    wx.chooseLocation({});
+  },
+
   qiehuan() {
-    console.log('111');
-    wx.openLocation({
-      latitude: 111,
-      longitude: 111,
-    })
+    // wx.openLocation({
+    //   latitude: 111,
+    //   longitude: 111,
+    // });
   },
   onLoad: function () {
     getLocation().then(location => {
       this.setData({location});
+
     });
+
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
